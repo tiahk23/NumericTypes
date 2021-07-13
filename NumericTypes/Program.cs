d152a4b0d86bf4a112601e416b28389de6de1ce6
Tiah Keefe <tetered12@gmail.com>
2021-07-12 20:17:23 -0500

    Add project files.

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
