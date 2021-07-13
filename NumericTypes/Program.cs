fca1d4c1597c84ea81a439a0497ef245b88b7beb
Tiah Keefe <tetered12@gmail.com>
2021-07-12 20:34:41 -0500

    Created a program that calculates the area of a rectangle.

--- NumericTypes/Program.cs
+++ NumericTypes/Program.cs
@@ -6,7 +6,12 @@ namespace NumericTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your rectangles length?");
+            string lengthOfRectangle = Console.ReadLine();
+            Console.WriteLine("What is your rectangles width?");
+            string widthOfRectangle = Console.ReadLine();
+            int area = int.Parse(lengthOfRectangle) * int.Parse(widthOfRectangle);
+            Console.WriteLine("The area of your rectangle is " + area);
         }
     }
 }
